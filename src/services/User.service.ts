a80dbc4d4720a65c870ad4df36d127465a63aef5
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-25 15:09:03 -0300

    feat: adicionada validação dos valores das propriedades do objeto user

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -1,10 +1,13 @@
 import { IUserDTO } from '../interfaces/User.interface';
 import UserModel from '../models/User.model';
+import UserValidation from '../validations/User.validation';
 
 export default class UserService {
   constructor(private model: UserModel) {}
 
   public async create(user: IUserDTO): Promise<void> {
+    UserValidation.validateUserCreationObject(user);
+
     await this.model.create(user);
   }
 }
