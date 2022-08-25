78a7a0ada0fccb7a39f2fb242f58195c919604af
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-25 15:48:31 -0300

    feat: adicionada função que encripta o password do user

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -8,13 +8,16 @@ import {
   DocumentData,
 } from 'firebase/firestore';
 import { IUserDTO } from '../interfaces/User.interface';
+import encryptPassword from '../helpers/encryptPassword';
 
 export default class UserModel {
   constructor(private firestore = getFirestore()) {}
 
   public async create(user: IUserDTO): Promise<void> {
+    const hashPassword = encryptPassword(user.email);
+
     const docRef = doc(this.firestore, `users/${user.email}`);
-    await setDoc(docRef, user);
+    await setDoc(docRef, { ...user, password: hashPassword });
   }
 
   public async getUser(email: string): Promise<DocumentData | null> {
