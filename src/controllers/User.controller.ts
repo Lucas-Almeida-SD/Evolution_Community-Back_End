82b08de7c3faa0b82ed219be2b9510c362c5163a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-25 11:35:09 -0300

    feat: retirado try/catch pois será utilizado o express-rescue para capturar os erros

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -1,23 +1,15 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import UserService from '../services/User.service';
 
 export default class UserController {
   constructor(private service: UserService) {}
 
-  public create = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): Promise<void> => {
-    try {
-      const user = req.body;
+  public create = async (req: Request, res: Response): Promise<void> => {
+    const user = req.body;
 
-      await this.service.create(user);
+    await this.service.create(user);
 
-      res.status(StatusCodes.CREATED).json({ message: 'Created', error: false });
-    } catch (err) {
-      next(err);
-    }
+    res.status(StatusCodes.CREATED).json({ message: 'Created', error: false });
   };
 }
