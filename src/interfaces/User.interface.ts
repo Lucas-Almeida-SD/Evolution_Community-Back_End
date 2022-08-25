60c392fd69f61ff946c99cae28fd040870aecf91
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-25 11:06:44 -0300

    feat: criada interface IUserDTO

---
+++ src/interfaces/User.interface.ts
@@ -0,0 +1,16 @@
+export interface IUserDTO {
+  fullname: string;
+  phone: string;
+  email: string;
+  CPF: number;
+  RG: number;
+  birthDate: Date;
+  password: string;
+  address: string;
+  houseNumber: number;
+  district: string;
+  city: string;
+  CEP: number;
+  complement: string;
+  plan: string;
+}
