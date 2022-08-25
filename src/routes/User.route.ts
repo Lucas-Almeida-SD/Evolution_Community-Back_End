836de5621c140dfdb56510ab9479f9a1fec567f3
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-25 11:37:52 -0300

    feat: criado "userRouter"que conterá as rotas do recurso "users"; criada rota "POST /users"

---
+++ src/routes/User.route.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import rescue from 'express-rescue';
+import UserController from '../controllers/User.controller';
+import UserService from '../services/User.service';
+import UserModel from '../models/User.model';
+
+const userRouter = Router();
+
+const userModel = new UserModel();
+const userService = new UserService(userModel);
+const userController = new UserController(userService);
+
+userRouter.post('/', rescue(userController.create));
+
+export default userRouter;
