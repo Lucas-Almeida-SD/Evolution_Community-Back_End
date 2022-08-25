c87d7276b565d1b1ca80c79edfb9b57f3af56f0a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-25 11:05:39 -0300

    feat: criado arquivo de configurações do firebase e inicializado a sua aplicação

---
+++ src/models/config/config.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import 'dotenv/config';
+
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} = process.env;
+
+initializeApp({
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+});
