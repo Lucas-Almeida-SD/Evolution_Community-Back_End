c04ddab9db4a7e364797c039137d2dd93a5c4e63
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-25 23:33:45 -0300

    feat: criada rota "PUT /users"

--- src/routes/User.route.ts
+++ src/routes/User.route.ts
@@ -3,6 +3,7 @@ import rescue from 'express-rescue';
 import UserController from '../controllers/User.controller';
 import UserService from '../services/User.service';
 import UserModel from '../models/User.model';
+import Middlewares from '../middlewares/middlewares';
 
 const userRouter = Router();
 
@@ -12,5 +13,6 @@ const userController = new UserController(userService);
 
 userRouter.post('/', rescue(userController.create));
 userRouter.post('/login', rescue(userController.login));
+userRouter.put('/', rescue(Middlewares.authentication), rescue(userController.edit));
 
 export default userRouter;
