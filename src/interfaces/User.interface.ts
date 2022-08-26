f8a8629a7d22677e6492c149f0b5d9211cde2079
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-25 23:23:29 -0300

    feat: criada interface IUserEdit

--- src/interfaces/User.interface.ts
+++ src/interfaces/User.interface.ts
@@ -17,3 +17,21 @@ export interface IUserDTO extends IUserLogin {
   complement: string;
   plan: string;
 }
+
+export interface IUserEdit {
+  email?: string;
+  password?: string;
+  fullname?: string;
+  phone?: string;
+  CPF?: string;
+  RG?: string;
+  birthDate?: string;
+  publicPlace?: string;
+  address?: string;
+  houseNumber?: number;
+  district?: string;
+  city?: string;
+  CEP?: string;
+  complement?: string;
+  plan?: string;
+}
